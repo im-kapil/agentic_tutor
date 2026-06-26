67ecf8110bc57e0b78c755c85049659842a498e0
im-kapil <infoatkapil@gmail.com>
2026-06-26 07:19:49 +0000

    Agent response modified

---
+++ apps/api/common/requrest_payloads.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+class UserQuery(BaseModel):
+    user_query: str
+    
--- apps/api/main.py
+++ apps/api/main.py
@@ -1,10 +1,8 @@
 from collections.abc import AsyncIterable
 from fastapi.sse import EventSourceResponse, ServerSentEvent
-from pydantic import BaseModel
 
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
-
+from apps.api.common.requrest_payloads import UserQuery
 from src.graph.state import state
 from src.graph.bootstrap import Bootstrap
 from src.runtime.orchestrator import Orchestrator, Workflow, Workflow
@@ -13,20 +11,6 @@ app = FastAPI(title="Agentic Mentorship Platform")
 
 import time
 
-async def data_streamer(agent_response):
-    print("stream started")    
-    for chunk in agent_response:
-        print("Chunk type:", chunk.content)
-        # print("repr:::", repr(chunk))
-        yield f"data: {chunk.content}\n\n"
-
-@app.get("/health")
-async def health():
-    return {"status": "healthy"}
-
-class UserQuery(BaseModel):
-    user_query: str
-    
 @app.post("/chat/stream", response_class=EventSourceResponse)
 async def stream_agent_response(user_query: UserQuery) -> AsyncIterable[str]:
     start = time.time()
@@ -37,59 +21,25 @@ async def stream_agent_response(user_query: UserQuery) -> AsyncIterable[str]:
 
     state["user_query"] = user_query.user_query
     
-    print("Received user query: ", state)
-
-    orchastrator = Orchestrator()
-    
-    orchastrator.run(Workflow.TUTOR, "state")
-    
-    print("workflow finished", time.time() - start)
-         
-    print("stream started")    
-    for chunk in state["agent_response"]:
-        print(chunk)
-        
-        # yield chunk.content
-        yield ServerSentEvent(data=str(chunk.content), event="Token")
-    
-    #TODO: Implement event publishing here
-
-# @app.post("/chat/stream", response_class=StreamingResponse)
-# async def stream_agent_response(user_query: UserQuery) -> AsyncIterable[str]:
-#     start = time.time()
-
-#     bootstrap = Bootstrap()
-#     bootstrap.register_agents()
-#     bootstrap.register_llms()
-
-#     state["user_query"] = user_query.user_query
-    
-#     print("Received user query: ", state)
-
-#     orchastrator = Orchestrator()
-    
-#     orchastrator.run(Workflow.TUTOR, "state")
-    
-#     print("workflow finished", time.time() - start)
-        
-#     return StreamingResponse(
-#         data_streamer(state["agent_response"]), 
-#         media_type='text/event-stream'
-#     )        
-
-@app.post("/agent/invoke", response_class=StreamingResponse)
-async def invoke_agent(user_query: UserQuery) -> AsyncIterable[str]:
-
-    # for line in user_query.user_query.splitlines():
-    #     yield line
-    bootstrap = Bootstrap()
-    bootstrap.register_agents()
-    bootstrap.register_llms()
-    
-    state["user_query"] = user_query.user_query
-
     orchastrator = Orchestrator()
     
     orchastrator.run(Workflow.TUTOR, state)
     
-    yield state["agent_response"]
+    print("workflow finished", time.time() - start)
+         
+    print("stream started")    
+    llm_full_response = ''
+    for chunk in state["agent_response"]:
+        print   (chunk)
+        llm_full_response += chunk.content
+        # yield chunk.content
+        # yield ServerSentEvent(data=str(chunk.content), event="Token")
+        yield ServerSentEvent(raw_data=chunk.content)
+        
+
+    
+    #TODO: Implement event publishing here
+    print(llm_full_response)
+    yield llm_full_response
+
+    
